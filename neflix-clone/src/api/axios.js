42c47a8c554a1bf27cca49616671242fbf7fd7ba
kimeonso <117335037+kimeonso@users.noreply.github.com>
2024-05-23 23:56:37 +0900

    fix

--- neflix-clone/src/api/axios.js
+++ neflix-clone/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   params: {
-    api_key: 'b',
+    api_key: '',
     language: 'ko-KR',
   },
 });
